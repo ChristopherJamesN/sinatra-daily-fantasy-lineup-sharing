fbebf997602b76d56391891a01a4c8ed5d74a9d4
ChristopherJamesN <nady.christopher@gmail.com>
2017-10-08 18:07:52 -0500

    Updated lineup and user controllers

--- app/controllers/lineups_controller.rb
+++ app/controllers/lineups_controller.rb
@@ -1,2 +1,62 @@
 class LineupsController < ApplicationController
+
+  get '/lineups' do
+    if session[:id] == nil
+      redirect to '/login'
+    end
+    @lineups = Lineup.all
+    erb :'lineups/lineups'
+  end
+
+  get '/lineups/new' do
+    if session[:id] == nil
+      redirect to '/login'
+    end
+    erb :'lineups/create_lineup'
+  end
+
+  post '/lineups' do
+    if params[:name] == ''
+      redirect to 'lineups/new'
+    end
+    @lineup = Lineup.create(name: params[:name], user_id: session[:id])
+    @user = User.find(@lineup.user_id)
+    @user.lineups << @lineup
+  end
+
+  get '/lineups/:id' do
+    if session[:id] == nil
+      redirect to '/login'
+    end
+    @lineup = Lineup.find(params[:id])
+    erb :'lineups/show_lineup'
+  end
+
+  get '/lineups/:id/edit' do
+    if session[:id] == nil
+      redirect to '/login'
+    end
+    @lineup = Lineup.find(params[:id])
+    if session[:id] != @lineup.user_id
+      redirect to '/lineups'
+    end
+    erb :'lineups/edit_lineup'
+  end
+
+  post '/lineups/:id' do
+    @lineup = Lineup.find(params[:id])
+    if params[:name] == ''
+      redirect to "lineups/#{@lineup.id}/edit"
+    end
+    @lineup.update(name: params[:name])
+  end
+
+  delete '/lineups/:id/delete' do
+    @lineup = Lineup.find(params[:id])
+    if session[:id] != @lineup.user_id
+      redirect to '/lineups'
+    end
+    @lineup.delete
+    redirect to '/lineups'
+  end
 end

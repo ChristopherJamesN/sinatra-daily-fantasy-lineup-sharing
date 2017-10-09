2af4d57ba0ae1afddf6c5785a87af4fa0b7b0a3c
ChristopherJamesN <nady.christopher@gmail.com>
2017-10-08 19:41:19 -0500

    Update the parameters for creating and editing a lineup

--- app/controllers/lineups_controller.rb
+++ app/controllers/lineups_controller.rb
@@ -19,7 +19,7 @@ class LineupsController < ApplicationController
     if params[:name] == ''
       redirect to 'lineups/new'
     end
-    @lineup = Lineup.create(name: params[:name], user_id: session[:id])
+    @lineup = Lineup.create(name: params[:name], quarterback: params[:quarterback], runningback_one: params[:runningback_one], runningback_two: params[:runningback_two], widereceiver_one: params[:widereceiver_one], widereceiver_two: params[:widereceiver_two], widereceiver_three: params[:widereceiver_three], tightend: params[:tightend], flex: params[:flex], defense: params[:defense], user_id: session[:id])
     @user = User.find(@lineup.user_id)
     @user.lineups << @lineup
   end
@@ -48,7 +48,7 @@ class LineupsController < ApplicationController
     if params[:name] == ''
       redirect to "lineups/#{@lineup.id}/edit"
     end
-    @lineup.update(name: params[:name])
+    @lineup.update(name: params[:name], quarterback: params[:quarterback], runningback_one: params[:runningback_one], runningback_two: params[:runningback_two], widereceiver_one: params[:widereceiver_one], widereceiver_two: params[:widereceiver_two], widereceiver_three: params[:widereceiver_three], tightend: params[:tightend], flex: params[:flex], defense: params[:defense])
   end
 
   delete '/lineups/:id/delete' do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,49 @@
 class UsersController < ApplicationController
 
-  get '/users/:id' do
-    if !logged_in?
-      redirect '/lineups'
+  get '/signup' do
+    if session[:id] != nil
+      redirect to '/lineups'
     end
+    erb :'users/create_user'
+  end
 
-    @user = User.find(params[:id])
-    if !@user.nil? && @user == current_user
-      erb :'users/show'
-    else
-      redirect '/lineups'
+  post '/signup' do
+    if params[:username] == ''
+      redirect to '/signup'
     end
+    if params[:email] == ''
+      redirect to '/signup'
+    end
+    if params[:password] == ''
+      redirect to '/signup'
+    end
+    @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+    session[:id] = @user.id
+    redirect to '/lineups'
+  end
+
+  get '/login' do
+    if session[:id] != nil
+      redirect to '/lineups'
+    end
+    erb :'users/login'
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user.authenticate(params[:password]) == false
+      redirect to 'login'
+    end
+    session[:id] = @user.id
+    redirect to '/lineups'
+  end
+
+  get '/logout' do
+    if session[:id] == nil
+      redirect to '/login'
+    end
+    session.clear
+    redirect to '/login'
   end
 
   get "/users/:slug" do
@@ -18,50 +51,8 @@ class UsersController < ApplicationController
     erb :'users/show'
   end
 
-  get '/signup' do
-    if !session[:user_id]
-      erb :'users/new'
-    else
-      redirect to '/lineups'
-    end
-  end
-
-  post '/signup' do
-    if params[:username] == "" || params[:password] == "" || params[:email] == ""
-      redirect to '/signup'
-    else
-      @user = User.create(:username => params[:username], :password => params[:password], :email => params[:email])
-      session[:user_id] = @user.id
-      redirect '/lineups'
-    end
-  end
-
-  get '/login' do
-    @error_message = params[:error]
-    if !session[:user_id]
-      erb :'users/login'
-    else
-      redirect '/lineups'
-    end
-  end
-
-  post '/login' do
-    user = User.find_by(:username => params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect "/lineups"
-    else
-      redirect to '/signup'
-    end
-  end
-
-  get '/logout' do
-    if session[:user_id] != nil
-      session.destroy
-      redirect to '/login'
-    else
-      redirect to '/'
-    end
+  get '/' do
+    erb :'/index'
   end
 
 end

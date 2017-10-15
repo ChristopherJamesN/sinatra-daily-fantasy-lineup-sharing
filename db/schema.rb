5c4a57c80db056645a68c29838195dc632dab01e
ChristopherJamesN <nady.christopher@gmail.com>
2017-10-14 19:34:40 -0500

    Updated readme

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2) do
+
+  create_table "lineups", force: :cascade do |t|
+    t.string "name"
+    t.string "quarterback"
+    t.string "runningback_one"
+    t.string "runningback_two"
+    t.string "widereceiver_one"
+    t.string "widereceiver_two"
+    t.string "widereceiver_three"
+    t.string "tightend"
+    t.string "flex"
+    t.string "defense"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.integer "lineup_id"
+  end
 
 end
